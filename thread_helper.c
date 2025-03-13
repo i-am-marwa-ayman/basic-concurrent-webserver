7de939fb401a68f37e2937410c1f07a15ba89afb
Marwa Ayman <i.am.marwa.ayman@gmail.com>
2025-03-13 17:32:05 +0200

    fix a typo / add some error handling / add destroy func

--- thread_helper.c
+++ thread_helper.c
@@ -7,27 +7,35 @@
 buffer_t *b;
 
 void init(int size) {
+    b = (buffer_t *)malloc(sizeof(buffer_t));
+    if (!b) {
+        perror("Failed to allocate buffer");
+        exit(1);
+    }
+
     b->size = size;
     b->front = -1;
     b->rear = -1;
-
-    b->buffer = (int)malloc(size * sizeof(int));
+    b->buffer = (int*)malloc(size * sizeof(int));
 
     pthread_mutex_init(&b->lock,NULL);
-    pthread_con_init(&b->fill,NULL);
-    pthread_con_init(&b->empty,NULL);
+    pthread_cond_init(&b->fill,NULL);
+    pthread_cond_init(&b->empty,NULL);
+
+    return;
 }
 
-void *master(int port){
+void *master(void *arg){
+    int port = *(int*) arg;
     int listen_fd = open_listen_fd_or_die(port);
 	while (1){
 		struct sockaddr_in client_addr;
 		int client_len = sizeof(client_addr);
 		int conn_fd = accept_or_die(listen_fd, (sockaddr_t *)&client_addr, (socklen_t *)&client_len);
 
-        Pthread_mutex_lock(&b->lock);
+        pthread_mutex_lock(&b->lock);
         while (b->rear == b->size - 1){
-            Pthread_cond_wait(&b->empty, b->lock);
+            pthread_cond_wait(&b->empty, &b->lock);
         }
         
         if(b->front == -1){
@@ -36,18 +44,33 @@ void *master(int port){
 
         b->buffer[++b->rear] = conn_fd;
 
-        Pthread_cond_signal(&b->fill);
-        Pthread_mutex_unlock(b->lock);
+        pthread_cond_signal(&b->fill);
+        pthread_mutex_unlock(&b->lock);
 	}
+    return NULL;
 }
 void *worker(){
-    Pthread_mutex_lock(&b->lock);
+    pthread_mutex_lock(&b->lock);
     while (b->front == -1 || b->front == b->rear + 1){
-        Pthread_cond_wait(&b->fill, &b->lock);
+        pthread_cond_wait(&b->fill, &b->lock);
     }
     int conn_fd = b->buffer[b->front++];
     request_handle(conn_fd);
 	close_or_die(conn_fd);
-    Pthread_cond_signal(&b->empty);
-    Pthread_mutex_unlock(&b->lock);
+    pthread_cond_signal(&b->empty);
+    pthread_mutex_unlock(&b->lock);
+    return NULL;
+}
+
+void destroy(){
+    if(b){
+        pthread_mutex_destroy(&b->lock);
+        pthread_cond_destroy(&b->empty);
+        pthread_cond_destroy(&b->fill);
+        if(!b->buffer){
+            free(b->buffer);
+        }
+        free(b);
+    }
+    return;
 }
