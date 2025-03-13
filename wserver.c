5efa8d533aa96a46d4f27937c8dcd555cd313bf7
Marwa Ayman <i.am.marwa.ayman@gmail.com>
2025-03-13 02:13:46 +0200

    add threads and buffer arg

--- wserver.c
+++ wserver.c
@@ -11,7 +11,8 @@ int main(int argc, char *argv[]) {
     int c;
     char *root_dir = default_root;
     int port = 10000;
-    
+    int thread_num = 1;
+	int buffer_size = 1;
     while ((c = getopt(argc, argv, "d:p:")) != -1)
 	switch (c) {
 	case 'd':
@@ -19,6 +20,12 @@ int main(int argc, char *argv[]) {
 	    break;
 	case 'p':
 	    port = atoi(optarg);
+	    break;
+	case 't':
+	    thread_num = atoi(optarg);
+	    break;
+	case 'b':
+	    buffer_size = atoi(optarg);
 	    break;
 	default:
 	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
@@ -30,14 +37,14 @@ int main(int argc, char *argv[]) {
 
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
-    while (1) {
-	struct sockaddr_in client_addr;
-	int client_len = sizeof(client_addr);
-	int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-	request_handle(conn_fd);
-	close_or_die(conn_fd);
-    }
-    return 0;
+	while (1){
+		struct sockaddr_in client_addr;
+		int client_len = sizeof(client_addr);
+		int conn_fd = accept_or_die(listen_fd, (sockaddr_t *)&client_addr, (socklen_t *)&client_len);
+		request_handle(conn_fd);
+		close_or_die(conn_fd);
+	}
+	return 0;
 }
 
 
