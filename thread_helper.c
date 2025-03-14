3b88a6e0216ac446f328501896fdde5d5026fe63
Marwa Ayman <i.am.marwa.ayman@gmail.com>
2025-03-15 00:53:35 +0200

    add circular queue functions

--- thread_helper.c
+++ thread_helper.c
@@ -6,6 +6,26 @@
 
 buffer_t *b;
 
+int is_full(){
+    return (b->cur_size == b->size) ? 1 : 0;
+}
+int is_empty(){
+    return (b->cur_size == 0) ? 1 : 0;
+}
+int fix_index(int index){
+    return (index + 1) % b->size;
+}
+void add(int new_element){
+    b->buffer[b->rear] = new_element;
+    b->rear = fix_index(b->rear);
+    b->cur_size++;
+}
+int pop(){
+    int front_element = b->buffer[b->front];
+    b->front = fix_index(b->front);
+    b->cur_size--;
+    return front_element;
+}
 void init(int size) {
     b = (buffer_t *)malloc(sizeof(buffer_t));
     if (!b) {
@@ -14,8 +34,9 @@ void init(int size) {
     }
 
     b->size = size;
-    b->front = -1;
-    b->rear = -1;
+    b->front = 0;
+    b->rear = 0;
+    b->cur_size = 0;
     b->buffer = (int*)malloc(size * sizeof(int));
 
     pthread_mutex_init(&b->lock,NULL);
@@ -34,16 +55,10 @@ void *master(void *arg){
 		int conn_fd = accept_or_die(listen_fd, (sockaddr_t *)&client_addr, (socklen_t *)&client_len);
 
         pthread_mutex_lock(&b->lock);
-        while (b->rear == b->size - 1){
+        while (is_full()){
             pthread_cond_wait(&b->empty, &b->lock);
         }
-        
-        if(b->front == -1){
-            b->front = 0;
-        }
-
-        b->buffer[++b->rear] = conn_fd;
-
+        add(conn_fd);
         pthread_cond_signal(&b->fill);
         pthread_mutex_unlock(&b->lock);
 	}
@@ -52,10 +67,10 @@ void *master(void *arg){
 void *worker(){
     while (1){
         pthread_mutex_lock(&b->lock);
-        while (b->front == -1 || b->front == b->rear + 1){
+        while (is_empty()){
             pthread_cond_wait(&b->fill, &b->lock);
         }
-        int conn_fd = b->buffer[b->front++];
+        int conn_fd = pop();
         pthread_cond_signal(&b->empty);
         pthread_mutex_unlock(&b->lock);
 
