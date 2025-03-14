a466f6edf400c026cb1cfd7f832077ddfbc1410c
Marwa Ayman <i.am.marwa.ayman@gmail.com>
2025-03-15 00:54:07 +0200

    update .h

--- thread_helper.h
+++ thread_helper.h
@@ -7,13 +7,23 @@ typedef struct {
     int *buffer;
     int front;
     int rear;
+    int cur_size;
 
     pthread_mutex_t lock;
     pthread_cond_t fill;
     pthread_cond_t empty;
 } buffer_t ;
+
+// threading functions
 void init(int size);
 void *master(void *arg);
 void *worker();
 void destroy();
+
+// queue functions
+int is_full();
+int is_empty();
+int fix_index(int index);
+void add(int new_element);
+int pop();
 #endif 
